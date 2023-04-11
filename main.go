8fe9ba1026e13ab687456feefdcf2e28b4dd8964
LiuWeijie <weijie.liu@intel.com>
2023-04-11 16:41:52 +0800

    Add trie router

--- main.go
+++ main.go
@@ -7,17 +7,22 @@ import (
 
 func main() {
 	r := engine.New()
-	r.GET("/", func(ctx *engine.Context) {
-		ctx.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	r.GET("/", func(c *engine.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
-	r.GET("/hello", func(ctx *engine.Context) {
-		ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Query("name"), ctx.Path)
+
+	r.GET("/hello", func(c *engine.Context) {
+		// expect /hello?name=geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
-	r.POST("/login", func(ctx *engine.Context) {
-		ctx.JSON(http.StatusOK, engine.H{
-			"username": ctx.PostForm("username"),
-			"password": ctx.PostForm("password"),
-		})
+
+	r.GET("/hello/:name", func(c *engine.Context) {
+		// expect /hello/geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *engine.Context) {
+		c.JSON(http.StatusOK, engine.H{"filepath": c.Param("filepath")})
 	})
 
 	r.Run(":9999")
