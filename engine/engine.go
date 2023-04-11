e0d6c37672795335fe644d0b1e2adfa992fc8400
weijie <997319474@qq.com>
2023-04-11 13:08:51 +0000

    Add route group control

--- engine/engine.go
+++ engine/engine.go
@@ -1,19 +1,48 @@
 package engine
 
 import (
+	"log"
 	"net/http"
 )
 
 type HandlerFunc func(*Context)
 
 type Engine struct {
-	router *router
+	*RouterGroup
+}
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc
+	router      *router
 }
 
 func New() *Engine {
-	return &Engine{
-		router: newRouter(),
+	engine := &Engine{}
+	engine.RouterGroup = &RouterGroup{router: newRouter()}
+	return engine
+}
+
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		router: group.router,
 	}
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.router.addRoute(method, pattern, handler)
+}
+
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
 func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
--- main.go
+++ main.go
@@ -7,23 +7,34 @@ import (
 
 func main() {
 	r := engine.New()
-	r.GET("/", func(c *engine.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	r.GET("/index", func(c *engine.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *engine.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
 
-	r.GET("/hello", func(c *engine.Context) {
-		// expect /hello?name=geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
+		v1.GET("/hello", func(c *engine.Context) {
+			// expect /hello?name=geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *engine.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *engine.Context) {
+			c.JSON(http.StatusOK, engine.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
 
-	r.GET("/hello/:name", func(c *engine.Context) {
-		// expect /hello/geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-
-	r.GET("/assets/*filepath", func(c *engine.Context) {
-		c.JSON(http.StatusOK, engine.H{"filepath": c.Param("filepath")})
-	})
+	}
 
 	r.Run(":9999")
 }
