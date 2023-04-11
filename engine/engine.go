9a83fbe75021b7cddbda623cae4ae62deb28a3c3
weijie <997319474@qq.com>
2023-04-11 14:19:45 +0000

    Add middlewares

--- engine/engine.go
+++ engine/engine.go
@@ -3,38 +3,44 @@ package engine
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 type HandlerFunc func(*Context)
 
 type Engine struct {
 	*RouterGroup
+	router *router
+	groups []*RouterGroup
 }
 
 type RouterGroup struct {
 	prefix      string
 	middlewares []HandlerFunc
-	router      *router
+	engine      *Engine
 }
 
 func New() *Engine {
-	engine := &Engine{}
-	engine.RouterGroup = &RouterGroup{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
 	return engine
 }
 
 func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
 	newGroup := &RouterGroup{
 		prefix: group.prefix + prefix,
-		router: group.router,
+		engine: engine,
 	}
+	engine.groups = append(engine.groups, newGroup)
 	return newGroup
 }
 
 func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
 	pattern := group.prefix + comp
 	log.Printf("Route %4s - %s", method, pattern)
-	group.router.addRoute(method, pattern, handler)
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
 func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
@@ -43,6 +49,10 @@ func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
 
 func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
 	group.addRoute("POST", pattern, handler)
+}
+
+func (group *RouterGroup) Use(middlewares ...HandlerFunc) {
+	group.middlewares = append(group.middlewares, middlewares...)
 }
 
 func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
@@ -62,6 +72,13 @@ func (engine *Engine) Run(addr string) (err error) {
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	var middlewares []HandlerFunc
+	for _, group := range engine.groups {
+		if strings.HasPrefix(req.URL.Path, group.prefix) {
+			middlewares = append(middlewares, group.middlewares...)
+		}
+	}
 	c := newContext(w, req)
+	c.handlers = middlewares
 	engine.router.handle(c)
 }
--- main.go
+++ main.go
@@ -2,38 +2,36 @@ package main
 
 import (
 	"gee/engine"
+	"log"
 	"net/http"
+	"time"
 )
+
+func onlyForV2() engine.HandlerFunc {
+	return func(c *engine.Context) {
+		// Start timer
+		t := time.Now()
+		// if a server error occurred
+		c.Fail(500, "Internal Server Error")
+		// Calculate resolution time
+		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
 
 func main() {
 	r := engine.New()
-	r.GET("/index", func(c *engine.Context) {
-		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
+	r.Use(engine.Logger()) // global midlleware
+	r.GET("/", func(c *engine.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
-	v1 := r.Group("/v1")
-	{
-		v1.GET("/", func(c *engine.Context) {
-			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
-		})
 
-		v1.GET("/hello", func(c *engine.Context) {
-			// expect /hello?name=geektutu
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-		})
-	}
 	v2 := r.Group("/v2")
+	v2.Use(onlyForV2()) // v2 group middleware
 	{
 		v2.GET("/hello/:name", func(c *engine.Context) {
 			// expect /hello/geektutu
 			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
 		})
-		v2.POST("/login", func(c *engine.Context) {
-			c.JSON(http.StatusOK, engine.H{
-				"username": c.PostForm("username"),
-				"password": c.PostForm("password"),
-			})
-		})
-
 	}
 
 	r.Run(":9999")
